1d8fffcd433b6cab30d3dcd7e42e917ddf4652d8
Hirwa-Joric <hirwajoric@gmail.com>
2023-06-02 20:12:20 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
       <Container className="d-flex align-items-center justify-content-center">
         <div className="w-100" style={{maxWidth:"400px"
         }}><Signup /></div>
-        
       </Container>
     </div>
   );
