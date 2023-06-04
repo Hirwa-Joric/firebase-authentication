908c81f171f2ec83c1d4b14938bb3ff1087b4160
Hirwa-Joric <hirwajoric@gmail.com>
2023-06-04 20:12:20 +0200

    updated

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,6 +1,7 @@
 import React,{useRef, useState} from 'react'
 import { Form, Button, Card , Alert} from "react-bootstrap"
 import { useAuth } from '../context/AuthContext'
+
 const Signup = () => {
   const emailRef = useRef()
   const passwordRef = useRef()
